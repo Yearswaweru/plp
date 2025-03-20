0e4aae91c6edc5e34d60e1b3d99441f606fd66ce
Yearswaweru <franklinewaweru19@gmail.com>
2025-03-21 01:59:14 +0300

    calculator

---
+++ calculator.py
@@ -0,0 +1,26 @@
+def main():
+    num1 = float(input("Enter the first number: "))
+    num2 = float(input("Enter the second number: "))
+    operation = input("Enter the operation (+, -, *, /): ")
+
+    
+    if operation == '+':
+        result = num1 + num2
+        print(f"{num1} + {num2} = {result}")
+    elif operation == '-':
+        result = num1 - num2
+        print(f"{num1} - {num2} = {result}")
+    elif operation == '*':
+        result = num1 * num2
+        print(f"{num1} * {num2} = {result}")
+    elif operation == '/':
+        if num2 != 0:
+            result = num1 / num2
+            print(f"{num1} / {num2} = {result}")
+        else:
+            print("Error: Division by zero is not allowed.")
+    else:
+        print("Error: Invalid operation. Please enter +, -, *, or /.")
+
+if __name__ == "__main__":
+    main()
--- index.py
+++ index.py
@@ -1,1 +1,3 @@
 print("Hello world")
+x = input("Whats your name?")
+print("x")
